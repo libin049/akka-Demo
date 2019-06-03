3143ac8518cc86ed2c4ef62d827f45a9535d32e8
libin <hsslb049@gmail.com>
2019-06-03 11:21:39 +0800

    add myWordCount

---
+++ src/main/java/org/akka/essentials/wc/mapreduce/example/myWordCount/CountActor.java
@@ -0,0 +1,94 @@
+package org.akka.essentials.wc.mapreduce.example.myWordCount;
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+
+import java.io.*;
+import java.util.*;
+
+class Result implements Serializable{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 5727560172917790458L;
+    private String word;
+    private int no_of_instances;
+
+    public Result(String word, int no_of_instances){
+        this.setWord(word);
+        this.setNoOfInstances(no_of_instances);
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setNoOfInstances(int no_of_instances) {
+        this.no_of_instances = no_of_instances;
+    }
+
+    public int getNoOfInstances() {
+        return no_of_instances;
+    }
+
+    public String toString(){
+        return word + ":" + no_of_instances;
+    }
+
+}
+
+public class CountActor extends UntypedActor {
+
+    private ActorRef reduceActor = null;
+
+    public CountActor(ActorRef inReduceActor) {
+        reduceActor = inReduceActor;
+    }
+    private void process(String fileName) {
+        try {
+            File file = new File(fileName);
+            FileInputStream in = new FileInputStream(file);
+            int fileLength = (int) file.length();
+            byte[] fileContext = new byte[fileLength];
+            in.read(fileContext);
+            reduceActor.tell(reduce(evaluateExpression(new String(fileContext))));
+
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
+    }
+
+    private List<Result> evaluateExpression(String context) {
+        List<Result> list = new ArrayList<Result>();
+        StringTokenizer parser = new StringTokenizer(context);
+        while (parser.hasMoreTokens()) {
+            String word = parser.nextToken().toLowerCase();
+            list.add(new Result(word, 1));
+        }
+        return list;
+    }
+    private HashMap<String, Integer> reduce(List<Result> list) {
+        HashMap<String, Integer> reducedMap = new HashMap<String, Integer>();
+        Iterator<Result> iter = list.iterator();
+        while (iter.hasNext()) {
+            Result result = iter.next();
+            if (reducedMap.containsKey(result.getWord())) {
+                Integer value = (Integer) reducedMap.get(result.getWord());
+                value++;
+                reducedMap.put(result.getWord(), value);
+            } else {
+                reducedMap.put(result.getWord(), Integer.valueOf(1));
+            }
+        }
+        return reducedMap;
+    }
+    @Override
+    public void onReceive(Object msg) throws Exception {
+        String message = (String) msg;
+        process(message);
+    }
+}
+
---
+++ src/main/java/org/akka/essentials/wc/mapreduce/example/myWordCount/Main.java
@@ -0,0 +1,30 @@
+package org.akka.essentials.wc.mapreduce.example.myWordCount;
+import akka.actor.*;
+import java.util.ArrayList;
+import java.util.List;
+import akka.actor.ActorSystem;
+public class Main {
+    static void fileReads() {
+
+    }
+    public static void main(String[] args) throws InterruptedException {
+        List<String> fileNames = new ArrayList<String>();
+        fileNames.add("/home/lb/Akka-Essentials/WordCountMapReduce/src/main/resources/Othello.txt");
+        fileNames.add("/home/lb/Akka-Essentials/WordCountMapReduce/src/main/resources/Othello1.txt");
+
+        ActorSystem system = ActorSystem.create("sys");
+        // create the aggregate Actor
+        final ActorRef aggregateActor = system.actorOf(new Props(AggregateActor.class));
+        for (int i = 0; i < fileNames.size(); i++) {
+            ActorRef countActor = system.actorOf(new Props(new UntypedActorFactory() {
+                public UntypedActor create() {
+                    return new CountActor(aggregateActor);
+                }
+            }), "countActor"+i);
+            String fileName = fileNames.get(i);
+            countActor.tell(fileName);
+        }
+        Thread.sleep(5000);
+        aggregateActor.tell("DISPLAY_LIST");
+    }
+}
